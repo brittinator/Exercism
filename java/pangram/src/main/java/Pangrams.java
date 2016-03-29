bc95f133eb8bbd7abac62a9d9f6580876bb557f0
Brittany L. Walentin <brittany.walentin@gmail.com>
2016-03-28 22:39:47 -0700

    Adds java pangram exercise.

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,11 +1,27 @@
+
+
 /**
  * Created by brittlwalentin on 3/21/16.
  */
 public class Pangrams {
 
 
-    public boolean isPangram(String str) {
-        return false;
+    public static boolean isPangram(String str) {
+        boolean isPangram = true; // until proven false
+
+        if (str.length() < 26)
+        { //26 letters in alphabet
+            return false;
+        }
+
+        str = str.toLowerCase();
+
+        for(char c='a'; c <='z'; c++) {
+            if(str.indexOf(c) == -1) {
+                isPangram = false;
+            }
+        }
+        return isPangram;
     }
 
 }
