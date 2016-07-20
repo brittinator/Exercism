e0954e1b9e3c7aa7d2d53c967b6ff3600a4bc6ef
Brittany Walentin <brittw@uber.com>
2016-07-20 10:36:26 -0700

    Adds refactored clock.go program.

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,20 +2,27 @@ package clock
 
 import "fmt"
 
-// must match `targetTestVersion` in clock_test.go.
-const testVersion = 3
-const MINUTES_IN_DAY = 24 * 60
+const testVersion = 4
+const minutesInADay = 24 * 60
 
 // Clock is only in minutes
 type Clock struct{ minute int }
 
+// New creates a Clock struct that is in valid format,
+// i.e. within the day 1440 minute frame
 func New(hr, min int) Clock {
 	minutes := 60*hr + min
+	for minutes >= minutesInADay {
+		minutes = minutes - minutesInADay
+	}
+	for minutes < 0 {
+		minutes = minutes + minutesInADay
+	}
 	return Clock{minutes}
 }
 
+// String prints out the clock time in hh:mm format as a string
 func (c Clock) String() string {
-	// prints out the clock time in hh:mm format as a string
 	// pad the output with leading zeros if not double digit
 	return fmt.Sprintf("%02d:%02d", Hour(c), Minute(c))
 }
@@ -23,7 +30,7 @@ func (c Clock) String() string {
 func (c Clock) Add(minutes int) Clock {
 	c.minute = c.minute + minutes
 	for c.minute < 0 {
-		c.minute = c.minute + MINUTES_IN_DAY
+		c.minute = c.minute + minutesInADay
 	}
 	return c
 }
